f38b2ab51e940ef2a1d70910a0ee1fb4cca2e078
Oskar Ingberg <oskar.ingberg@ninetech.com>
2017-03-14 14:23:02 +0100

    Emit newMessage on create Message'

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Oskar',
-        text: 'Test, test and test'
-    });
 });
 
 socket.on('disconnect', function(){
